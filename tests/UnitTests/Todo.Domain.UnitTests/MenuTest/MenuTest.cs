09a266186d6cbb62fe15decf0b44ba76ed8c38ac
danilosantanaa <38994152+danilosantanaa@users.noreply.github.com>
2023-11-28 18:16:37 -0300

    Teste de Update de Menu

--- tests/UnitTests/Todo.Domain.UnitTests/MenuTest/MenuTest.cs
+++ tests/UnitTests/Todo.Domain.UnitTests/MenuTest/MenuTest.cs
@@ -7,19 +7,37 @@ namespace Todo.Domain.UnitTests.MenuTest;
 
 public class MenuTest
 {
+    [Fact]
     public void Menu_Deve_Ser_Criado()
     {
         // Arrange
         string nome = "Terefa do dia";
-        string iconUrl = "teste/teste.jpg";
-
-        // Act
+        string iconUrl = "teste.jpg";
         var menuId = MenuId.Create();
 
+        // Act
         var menu = Menu.Create(menuId, nome, iconUrl);
 
         // Assert
         menu.Nome.Should().Be(nome);
         menu.IconUrl.Should().Be(iconUrl);
+    }
+
+    [Fact]
+    public void Menu_Deve_Ser_Atualizado()
+    {
+        // Arrange
+        string nome = "Tarefa do dia";
+        string iconUrl = "teste.png";
+        Menu menu = Menu.Create(MenuId.Create(), nome, iconUrl);
+        MenuId menuId = menu.Id;
+
+        // Act
+        menu.Update(nome, iconUrl);
+
+        // Assert
+        menu.Nome.Should().Be(nome);
+        menu.IconUrl.Should().Be(iconUrl);
+        menu.Id.Should().Be(menuId);
     }
 }
